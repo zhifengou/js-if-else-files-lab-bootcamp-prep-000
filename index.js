4a36e47ef8147786c467722520a8ed480da91d2d
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-13 14:35:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var height = 70
 module.exports = { name, height
 }
 
-if(name==="Susan"){
-  console.log("The name is correct")
-}
+// if(name==="Susan"){
+//   console.log("The name is correct")
+// }
 else{
   console.log("Expected the name to be Susan")
 }
